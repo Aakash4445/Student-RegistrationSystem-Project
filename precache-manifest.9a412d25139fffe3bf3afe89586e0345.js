e528b3695dd5c1d8327cdb69757df7246a870efe
aakash rokade <aakashrokade071@gmail.com>
2025-01-02 20:33:54 +0530

    Updates

---
+++ precache-manifest.9a412d25139fffe3bf3afe89586e0345.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ddd87a01ce551bf0ccbf242db1ae93f",
+    "url": "/Student-RegistrationSystem-Project/index.html"
+  },
+  {
+    "revision": "9c79c44f652b8f0b4afc",
+    "url": "/Student-RegistrationSystem-Project/static/css/main.4122c4c3.chunk.css"
+  },
+  {
+    "revision": "1fef3c9ea5c778b0a2d5",
+    "url": "/Student-RegistrationSystem-Project/static/js/2.63c7d5f3.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/Student-RegistrationSystem-Project/static/js/2.63c7d5f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c79c44f652b8f0b4afc",
+    "url": "/Student-RegistrationSystem-Project/static/js/main.856e369d.chunk.js"
+  },
+  {
+    "revision": "5681acf7af92b8621bc6",
+    "url": "/Student-RegistrationSystem-Project/static/js/runtime-main.92972ba2.js"
+  }
+]);
